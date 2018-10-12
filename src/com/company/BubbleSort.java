eff455637592fdbcc99d6f755563f10400f0997d
NikitaGaydukMinsk <NikitaGaydukMinsk@gmail.com>
2018-10-12 14:22:47 +0300

    Delete out of array exception, add part of sort method

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -2,23 +2,34 @@ package com.company;
 import java.util.Random;
 
 public class BubbleSort {
-    int arraySize;
-    int arrayInt[] = new int[arraySize];
+
+    int arrayInt[];
 
     BubbleSort(int size){
-        arraySize = size;
+        arrayInt = new int[size];
     }
 
     void initializeRandomNumbers(){
         Random random = new Random();
-        for(int index = 0; index<arraySize; index++){
-            arrayInt[index]=random.nextInt(100)+1;
+        for(int index = 0; index<arrayInt.length; index++){
+            arrayInt[index]=random.nextInt(10)+1;
+        }
+    }
+
+    void sortArray(){
+        for(int begin = 0; begin<arrayInt.length-1; begin++){
+            if(arrayInt[begin]>arrayInt[begin+1]){
+                int tmp = arrayInt[begin];
+                arrayInt[begin] = arrayInt[begin+1];
+                arrayInt[begin+1] = tmp;
+            } else continue;
         }
     }
 
     void showArray(){
-        for(int index = 0; index<arraySize; index++){
-            System.out.println(arrayInt[index]);
+        System.out.println("Array: ");
+        for(int index = 0; index<arrayInt.length; index++){
+        System.out.println(arrayInt[index]);
         }
     }
 }
